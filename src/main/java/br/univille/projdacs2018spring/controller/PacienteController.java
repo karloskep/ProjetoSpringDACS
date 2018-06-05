787251b57fce1e2fec3e9ec2c36ca1f7c88eb403
KARLOS EDUARDO PFAFFENZELLER <karlos.pfaffenzeller@univille.br>
2018-06-04 22:01:19 -0300

    interface e banco de dados

--- src/main/java/br/univille/projdacs2018spring/controller/PacienteController.java
+++ src/main/java/br/univille/projdacs2018spring/controller/PacienteController.java
@@ -1,31 +1,26 @@
 package br.univille.projdacs2018spring.controller;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.univille.projdacs2018spring.model.Paciente;
+import br.univille.projdacs2018spring.repository.PacienteRepository;
 
 @Controller
 @RequestMapping("/paciente")
 public class PacienteController {
-
-@GetMapping("")
-public ModelAndView index() {
-	
-    List<Paciente> listaPaciente = new ArrayList<Paciente>();
     
-    Paciente p1 = new Paciente();
-    p1.setNome("Zezinho");
-    p1.setSexo("Masculino");
-    
-    listaPaciente.add(p1);
-    
-    return new ModelAndView("paciente/index","listapac",listaPaciente);
-}
+    @Autowired
+    private PacienteRepository pacienteRepository;
 
-
+    @GetMapping("")
+    public ModelAndView index() {
+        List<Paciente> listaPaciente = this.pacienteRepository.findAll();
+        
+        return new ModelAndView("paciente/index","listapac",listaPaciente);
+    }
 }
